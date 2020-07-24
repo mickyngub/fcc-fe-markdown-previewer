f46d035a4cdcbb191b9de4da49d0b5db36b960f8
mickyngub <57980116+mickyngub@users.noreply.github.com>
2020-07-24 16:07:54 +0700

    passed the require tests

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -1,27 +1,29 @@
 import React, { useState } from "react";
+import DOMPurify from "dompurify";
 
 const marked = require("marked");
 const TextArea = () => {
-  const [text, setText] = useState("");
+  const [text, setText] = useState(
+    "# Welcome to my React Markdown Previewer!\n## This is a sub-heading... \n`<div></div>`, between 2 backticks.\n```\nlet s = 5// this is multi-line code:\n```\n**bold**There's also [links](https://www.freecodecamp.com), and\n> Block Quotes!\n- And of course there are lists.\n- Some are bulleted.\n1. And there are numbererd lists too.1. Use just 1s if you want! 1. But the list goes on...![React Logo w/ Text](https://goo.gl/Umyytc)"
+  );
   const changeText = (e) => {
     setText(e.target.value);
   };
+  let dirty = marked(text);
+  let clean = DOMPurify.sanitize(dirty);
   return (
     <React.Fragment>
-      <textarea id="editor" onChange={changeText}>
-        {/* <div>
-          <h1>This is h1</h1>
-          <h2>This is h2</h2>
-          <a href="www.google.com" target="_blank" rel="noopener">
-            This is link
-          </a>
-        </div> */}
+      <textarea
+        id="editor"
+        onChange={changeText}
+        style={{ width: 500, height: 500 }}
+      >
+        {text}
       </textarea>
-      ;
-      <div id="preview">
+
+      <div>
+        <div id="preview" dangerouslySetInnerHTML={{ __html: clean }}></div>
         <p>Result Text</p>
-        {/* {text} */}
-        {marked(text)}
       </div>
     </React.Fragment>
   );
