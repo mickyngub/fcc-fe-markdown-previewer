df698ebfd86d77494f8e13d7fa68c3681202b662
mickyngub <57980116+mickyngub@users.noreply.github.com>
2020-07-24 15:15:08 +0700

    ✍🏼add marked

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
+const marked = require("marked");
 const TextArea = () => {
-  return <textarea></textarea>;
+  const [text, setText] = useState("");
+  const changeText = (e) => {
+    setText(e.target.value);
+  };
+  return (
+    <React.Fragment>
+      <textarea id="editor" onChange={changeText}>
+        {/* <div>
+          <h1>This is h1</h1>
+          <h2>This is h2</h2>
+          <a href="www.google.com" target="_blank" rel="noopener">
+            This is link
+          </a>
+        </div> */}
+      </textarea>
+      ;
+      <div id="preview">
+        <p>Result Text</p>
+        {/* {text} */}
+        {marked(text)}
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default TextArea;
